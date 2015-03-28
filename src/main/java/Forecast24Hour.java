8d0a2487dcb01fc8226cacce8b6533cbecad1676
Evan Finnigan <efinnig@uwo.ca>
2015-03-27 20:53:16 -0400

    Forecast 24h Date format & Layout change
    Made it pretty :smile_cat:

--- src/main/java/Forecast24Hour.java
+++ src/main/java/Forecast24Hour.java
@@ -1,10 +1,15 @@
 package main.java;
+
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -50,31 +55,37 @@ public class Forecast24Hour extends TransparentPanel {
 		String[] skyConditionArray = data24h.getSkyCondition24h();
 		String[] descriptionArray = data24h.getDescription24h();
 		Image[] imgArray = data24h.getIcon24h();
-		
+
 		double[] tempArray;
-		
-		if (preferences.getTempUnit().equalsIgnoreCase("F")){
+
+		if (preferences.getTempUnit().equalsIgnoreCase("F")) {
 			tempArray = data24h.getTemp24hF();
 		} else {
 			tempArray = data24h.getTemp24h();
 		}
-		
+
 		int cnt = data24h.getCNT24h();
 		if (cnt > 8) {
 			cnt = 8;
 		}
-		
+
+		Date date = new Date();
 		for (int i = 0; i < cnt; i++) {
-			int j = (i + 1) * 3;
-			
-			String temp = descriptionArray[i].substring(0, 1).toUpperCase() + descriptionArray[i].substring(1);
-			wdlabel[i] = new JLabel(j + " hrs: " + temp);
+			date.setHours(date.getHours() + 3);
+			DateFormat format = new SimpleDateFormat("EEEEEEEE, h:mm a");
+			TimeZone est = TimeZone.getTimeZone("America/Toronto");
+			format.setTimeZone(est);
+			String j = format.format(date);
+
+			String temp = descriptionArray[i].substring(0, 1).toUpperCase()
+					+ descriptionArray[i].substring(1);
+			wdlabel[i] = new JLabel(j);
 			wdlabel[i].setFont(minorfont);
 
 			sclabel[i] = new JLabel("Sky:  " + skyConditionArray[i]);
 			sclabel[i].setFont(minorfont);
 
-			if (preferences.getTempUnit().equalsIgnoreCase("F")){
+			if (preferences.getTempUnit().equalsIgnoreCase("F")) {
 				templabel[i] = new JLabel(tempArray[i] + "\u00b0" + "F");
 				templabel[i].setFont(minorfont);
 			} else {
@@ -83,14 +94,18 @@ public class Forecast24Hour extends TransparentPanel {
 			}
 
 			imagelabel[i] = new JLabel(new ImageIcon(imgArray[i]));
-			imagelabel[i].setPreferredSize(new Dimension(5,5));
+			imagelabel[i].setPreferredSize(new Dimension(5, 5));
 		}
 
 		for (int i = cnt; i < 8; i++) {
-			
-			int j = (i + 1)*3;
-			
-			wdlabel[i] = new JLabel(j + " hours:");
+
+			date.setHours(date.getHours() + 3);
+			DateFormat format = new SimpleDateFormat("EEEEEEEE, h:mm a");
+			TimeZone est = TimeZone.getTimeZone("America/Toronto");
+			format.setTimeZone(est);
+			String j = format.format(date);
+
+			wdlabel[i] = new JLabel(j);
 			wdlabel[i].setFont(minorfont);
 			sclabel[i] = new JLabel("-");
 			sclabel[i].setFont(minorfont);
@@ -99,7 +114,7 @@ public class Forecast24Hour extends TransparentPanel {
 			imagelabel[i] = new JLabel("Data");
 			imagelabel[i].setFont(minorfont);
 		}
-	
+
 		this.weatherdescription_l = wdlabel;
 		this.skycondition_l = sclabel;
 		this.temp_l = templabel;
@@ -113,25 +128,25 @@ public class Forecast24Hour extends TransparentPanel {
 		this.setLayout(panelayout);
 		TransparentPanel[] blocks = new TransparentPanel[8];
 		for (int i = 0; i < 8; i++) {
-			blocks[i] = new Create24hourBlock(temp_l[i],image_l[i],skycondition_l[i],weatherdescription_l[i]);
+			blocks[i] = new Create24hourBlock(weatherdescription_l[i], temp_l[i], image_l[i],
+					skycondition_l[i]);
 		}
 		this.add(new Layout24hour(blocks));
-}
-	
+	}
 
 	// Test
-/*	public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		InputTest t = new InputTest("paris");
 		WeatherData d = new WeatherData(t);
 		WeatherPreferences p = new WeatherPreferences();
-		CopyOfForecast24Hour test = new CopyOfForecast24Hour(d,p);
+		CopyOfForecast24Hour test = new CopyOfForecast24Hour(d, p);
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.add(test);
 		frame.setVisible(true);
 		frame.pack();
 		frame.setTitle("24 Hour Weather Forecast for London, Canada");
-	}*/
+	}
 
 }
